083e3bddc1121769e3a83c99e833c39520cc2621
IsmaelV <ismaelvillegas3@gmail.com>
2021-10-30 00:15:19 -0700

    Send form to python endpoint and handle information | Created LayoverCalendar class to organize calendars

---
+++ LayoverCalendar.py
@@ -0,0 +1,14 @@
+class LayoverCalendar:
+	def __init__(self, name, init_user_email, init_user_name, meeting_type):
+		self.name = name
+		self.users = {init_user_email: init_user_name}
+		self.meeting_type = meeting_type
+
+	def getName(self):
+		return self.name
+
+	def getUsers(self):
+		return self.users.keys()
+
+	def getMeetingType(self):
+		return self.meeting_type
--- server.py
+++ server.py
@@ -1,13 +1,47 @@
+import string
+import random
+
 from flask import Flask
 from flask import render_template, redirect
 from flask import Response, request, jsonify
 
+from LayoverCalendar import LayoverCalendar
+
 app = Flask(__name__)
+
+meeting_db = dict()
 
 
 @app.route('/')
 def home():
 	return render_template('setup-landing.html')
+
+
+@app.route('/handle_meeting_creation', methods=['POST'])
+def handle_meeting_creation():
+	# Read form results
+	meeting_name = request.form['meeting_name']
+	display_name = request.form['display_name']
+	email = request.form['email']
+	meeting_type = request.form['meeting_type']
+
+	# Create initial calendar
+	myCalendar = LayoverCalendar(meeting_name, email, display_name, meeting_type)
+
+	# Create unique Calendar ID
+	calendarID = getRandomHash()
+	myKeys = meeting_db.keys()
+	while calendarID in myKeys:
+		calendarID = getRandomHash()
+
+	meeting_db[calendarID] = myCalendar
+	print(meeting_db)
+	return "Finished"
+
+
+def getRandomHash():
+	available = string.ascii_letters + string.digits
+	return ''.join(random.choice(available) for i in range(6))
 
 
 if __name__ == "__main__":
