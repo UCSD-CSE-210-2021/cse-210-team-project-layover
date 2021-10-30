01986697ed5210762f818b9628664c26b9af0b1f
IsmaelV <ismaelvillegas3@gmail.com>
2021-10-30 11:29:11 -0700

    Added start/end date show/hide functionality | Added JQuery

--- static/setup-landing.js
+++ static/setup-landing.js
@@ -1,3 +1,14 @@
 // This file will contain any javascript computation that we need for the setup-landing html page
 // It is here where we will grab the setup information to inform what our scheduling will be:
 //     in-person, remote, both; fixed dates, general week; etc.
+$(document).ready(function() {
+    $("#general_week").click(function() {
+        $("#start_date_div").addClass("hideDiv");
+        $("#end_date_div").addClass("hideDiv");
+    });
+
+    $("#specific_date").click(function() {
+        $("#start_date_div").removeClass("hideDiv");
+        $("#end_date_div").removeClass("hideDiv");
+    });
+})
