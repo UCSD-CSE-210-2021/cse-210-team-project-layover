45f37ec2a97cd498e516f2ae9156e047daa6fe64
Eric Yeh <epyeh@ucsd.edu>
2021-12-03 14:32:04 -0800

    removing redundant files

--- layover/static-old/constants.js
+++
@@ -1,1 +0,0 @@
-const numCol = 8;
--- layover/static-old/scheduling-landing.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	var all_data = jQuery.parseJSON(data)
-	$("#results_btn").attr("href", "/results/" + all_data.meeting_id);
-	$("#meeting_id").attr("value", all_data.meeting_id);
-	$("#meeting_name").html(all_data.name)
-});
